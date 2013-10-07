e36e759df3b6d73b8dc6123dc7e7f8b98c74989f
vrde <agranzot@gmail.com>
2013-10-07 03:04:57 +0200

    add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+﻿# -*- coding: utf-8 -*-
+
+from setuptools import setup, find_packages
+
+setup(
+    name         = 'MorgenJS',
+    version      = '0.1',
+    description  = 'A simple tool to help your JS development',
+    url          = '',
+
+    author       = 'Alberto Granzotto',
+    author_email = 'a@urli.st',
+
+    packages     = find_packages(),
+
+    entry_points = {
+        'console_scripts': [
+            'morgencli = server:main',
+        ],
+    },
+
+    install_requires = [
+        'tornado',
+        'watchdog'
+    ]
+)
+
